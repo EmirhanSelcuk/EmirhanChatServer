d5a77ddedb6aac7c4db073701ab57fcbb076a1e1
emirhanselcuk <selcukemirhan88@gmail.com>
2024-11-08 10:35:19 +0300

    Add project files.

---
+++ EmirhanChatServer.WebAPI/Context/AppLicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmirhanChatServer.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmirhanChatServer.WebAPI.Context
+{
+    public class AppLicationDbContext : DbContext
+    {
+        public AppLicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+    }
+}
